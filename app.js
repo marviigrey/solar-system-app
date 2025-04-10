bbf51240d14dd3d2b789c59226673f44cae917c7
marviigrey <marvellousfrederick@gmail.com>
2025-04-10 19:28:42 +0100

    changed app.js

--- app.js
+++ app.js
@@ -19,10 +19,12 @@ mongoose.connect(process.env.MONGO_URI, {
 }, function(err) {
     if (err) {
         console.log("error!! " + err)
+        process.exit(1);  // 👈 Kill the process if MongoDB isn't reachable
     } else {
-      //  console.log("MongoDB Connection Successful")
+        console.log("MongoDB Connection Successful");
     }
-})
+});
+
 
 var Schema = mongoose.Schema;
 
